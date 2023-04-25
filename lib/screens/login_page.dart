3c16508c20dc29fe342a3566bfce6f93daf344cd
eodevelop <eodevelop@naver.com>
2023-04-25 21:07:32 +0900

    1. Login 페이지 디자인 변경

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -4,19 +4,35 @@ import 'package:intranet/screens/board_home_page.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    InputDecoration textFieldDecoration = InputDecoration(
+      filled: true,
+      fillColor: Colors.white,
+      hintText: '',
+      hintStyle: TextStyle(color: Colors.grey),
+      contentPadding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(30),
+        borderSide: BorderSide(color: Theme.of(context).primaryColor),
+      ),
+    );
+
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
-              decoration: InputDecoration(hintText: 'Username'),
+              decoration: textFieldDecoration.copyWith(hintText: 'Username'),
             ),
+            SizedBox(height: 20),
             TextField(
-              decoration: InputDecoration(hintText: 'Password'),
+              decoration: textFieldDecoration.copyWith(hintText: 'Password'),
               obscureText: true,
             ),
+            SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushReplacement(
